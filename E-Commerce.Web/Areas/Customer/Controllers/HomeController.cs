c1213f2343e4a4388dd11bb6955dbb9b6a1085ab
Ahmed Alian <anmad.alian689@gmail.com>
2025-08-19 04:54:14 +0300

    about us page design

--- E-Commerce.Web/Areas/Customer/Controllers/HomeController.cs
+++ E-Commerce.Web/Areas/Customer/Controllers/HomeController.cs
@@ -24,6 +24,11 @@ namespace E_Commerce.Web.Areas.Customer.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
